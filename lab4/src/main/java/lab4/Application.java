a3b8c3e6ee2b516fdb6d59fb5568c86174287ed6
ivn <ivnsimakhin@gmail.com>
2017-10-19 16:47:57 +0300

    ...

---
+++ lab4/src/main/java/lab4/Application.java
@@ -0,0 +1,170 @@
+package lab4;
+
+import org.joml.Matrix4f;
+import org.joml.Vector4f;
+import org.lwjgl.BufferUtils;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.system.MemoryStack;
+
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.stackPush;
+
+public class Application {
+
+    private long win;
+    private boolean rot = false;
+    private Matrix4f m;
+    private double aspect;
+    private int initWidth, initHeight;
+
+    public Application(int w, int h) {
+        m = new Matrix4f().identity();
+        aspect = 1;
+        initWidth = w;
+        initHeight = h;
+    }
+
+    private void start() {
+        init();
+        loop();
+
+        glfwFreeCallbacks(win);
+        glfwDestroyWindow(win);
+
+        // Terminate GLFW and free the error callback
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+
+    private void init() {
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        if (!glfwInit()) {
+            throw new IllegalStateException("Unable to init GLFW");
+        }
+
+        glfwDefaultWindowHints();
+        // window hint = window property?
+        glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+        win = glfwCreateWindow(600, 600, "Лабораторная работа №4", 0, 0);
+        glfwSetWindowSizeLimits(win, 600, 600, GLFW_DONT_CARE, GLFW_DONT_CARE);
+        glfwSetKeyCallback(win, (window, key, scancode, action, mods) -> {
+            if (key == GLFW_KEY_SPACE && action == GLFW_PRESS) {
+                rot = !rot; // We will detect this in the rendering loop
+            }
+            if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+                glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
+            }
+            if (key == GLFW_KEY_UP && action == GLFW_PRESS) {
+                System.out.println("UP");
+                m.rotateX((float) Math.toRadians(-30));
+            }
+            if (key == GLFW_KEY_DOWN && action == GLFW_PRESS) {
+                System.out.println("DOWN");
+                m.rotateX((float) Math.toRadians(30));
+            }
+            if (key == GLFW_KEY_LEFT && action == GLFW_PRESS) {
+                System.out.println("LEFT");
+                m.rotateY((float) Math.toRadians(30));
+            }
+            if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS) {
+                System.out.println("RIGHT");
+                m.rotateY((float) Math.toRadians(-30));
+            }
+        });
+
+        glfwSetFramebufferSizeCallback(win, (win, w, h) -> {
+            //aspect = Math.max(w/600.,h/600.);
+            //m.scale((float) aspect);
+            glViewport(0, 0, w, h);
+        });
+
+        try (MemoryStack stack = stackPush()) {
+            IntBuffer pWidth = stack.mallocInt(1); // int*
+            IntBuffer pHeight = stack.mallocInt(1); // int*
+
+            // Get the window size passed to glfwCreateWindow
+            glfwGetWindowSize(win, pWidth, pHeight);
+
+            // Get the resolution of the primary monitor
+            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+            // Center the window
+            glfwSetWindowPos(
+                    win,
+                    (vidmode.width() - pWidth.get(0)) / 2,
+                    (vidmode.height() - pHeight.get(0)) / 2
+            );
+        }
+
+        glfwMakeContextCurrent(win);
+        // Enable v-sync
+        glfwSwapInterval(0);
+
+        // Make the window visible
+        glfwShowWindow(win);
+    }
+
+    private void loop() {
+        // This line is critical for LWJGL's interoperation with GLFW's
+        // OpenGL context, or any context that is managed externally.
+        // LWJGL detects the context that is current in the current thread,
+        // creates the GLCapabilities instance and makes the OpenGL
+        // bindings available for use.
+        GL.createCapabilities();
+
+        glViewport(0, 0, 600, 600);
+
+        // Set the clear color
+        glClearColor(245.0f / 255, 222.0f / 255, 179.0f / 255, 0.0f);
+
+        glEnable(GL_DEPTH_TEST);
+        // Run the rendering loop until the user has attempted to close
+        // the window or has pressed the ESCAPE key.
+
+        Vector4f center = new Vector4f(0, 0, 0, 1);
+        Vector4f axisX = new Vector4f(1, 0, 0, 1);
+        Vector4f axisY = new Vector4f(0, 1, 0, 1);
+        Vector4f axisZ = new Vector4f(0, 0, 1, 1);
+        FloatBuffer fb = BufferUtils.createFloatBuffer(16);
+        while (!glfwWindowShouldClose(win)) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+            byte zero = 127;
+            // Load transform matrix
+            glLoadMatrixf(m.get(fb));
+
+            glColor3b(zero, zero, zero);
+
+            glBegin(GL_LINE_LOOP);
+                glVertex2f(center.x, center.y);
+                glVertex2f(axisX.x, axisX.y);
+            glEnd();
+
+            glBegin(GL_LINE_LOOP);
+                glVertex2f(center.x, center.y);
+                glVertex2f(axisY.x, axisY.y);
+            glEnd();
+
+            glBegin(GL_LINE_LOOP);
+                glVertex2f(center.x, center.y);
+                glVertex2f(axisZ.x, axisZ.y);
+            glEnd();
+
+            glfwSwapBuffers(win); // swap the color buffers
+            glfwPollEvents();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Application(600, 600).start();
+    }
+}
