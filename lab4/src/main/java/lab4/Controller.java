2afc058965379c0f42f071a7a59f6ff18aec07dc
ivan <abc@abc.com>
2017-10-30 15:02:13 +0300

    TODO: add controller

--- lab4/src/main/java/lab4/Controller.java
+++ lab4/src/main/java/lab4/Controller.java
@@ -1,8 +1,18 @@
 package lab4;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
 
 public class Controller {
 
 
+    @FXML
+    private Slider r;
+
+    @FXML
+    private void initialize() {
+        r.valueProperty().addListener(((observable, oldValue, newValue) -> {
+            System.out.println(newValue.doubleValue());
+        }));
+    }
 }
